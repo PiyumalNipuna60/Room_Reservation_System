9cf0f6ed216417e644234fa00e78850b0c3d4d46
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-24 22:22:09 +0530

    AdminDashBoardController Class Create

---
+++ Room_Reservation_System/src/controller/AdminDashBoardController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AdminDashBoardController {
+    public Button btnIncome;
+    public Button btnBack;
+    public Label lblRealTime;
+    public Label lblRealDate;
+    public Button btnManageRoom;
+    public Button btnMealPlan;
+
+    public void manageMealOnAction(ActionEvent actionEvent) {
+    }
+
+    public void manageRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void manageIncomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void BackOnActio9n(ActionEvent actionEvent) {
+    }
+}
