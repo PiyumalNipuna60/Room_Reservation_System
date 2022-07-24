008e61382c41212cec864d2c33224fc87ef1b4a3
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-24 22:13:15 +0530

    HomePageController Class Create

---
+++ Room_Reservation_System/src/controller/HomePageController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class HomePageController {
+    public Button btnAdmin;
+    public Button btnReceipt;
+
+    public void AdminOnActiojnh(ActionEvent actionEvent) {
+    }
+
+    public void ReceptionistOnAction(ActionEvent actionEvent) {
+    }
+}
