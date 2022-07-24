46613a2017e6c0643aa6eef5528c706f76474ffe
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-24 22:10:42 +0530

    LogInAdminController Class Import

--- Room_Reservation_System/src/controller/LogInAdminController.java
+++ Room_Reservation_System/src/controller/LogInAdminController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
 public class LogInAdminController {
+    public PasswordField txtAdminUserName;
+    public Button btnAdminCancel;
+    public Button btnAdminLogIn;
+    public PasswordField txtAdminPassword;
+
+    public void AdmintLogInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelAdminOnAction(ActionEvent actionEvent) {
+    }
 }
