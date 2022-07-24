5dcac47802591d23c749e68d9cd9403d2bfbda78
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-24 22:24:38 +0530

    UserDashBoard Class Create

---
+++ Room_Reservation_System/src/controller/UserDashBoardController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UserDashBoardController {
+    public TextField txtId;
+    public Button btnManageReservation;
+    public Button btnBack;
+    public Label lblRealTime;
+    public Label lblRealDate;
+    public Button btnReserveRoom;
+    public ComboBox comboSelectMealPlan;
+    public ComboBox comboSelectRoom;
+    public Button btnMaintenance;
+    public Button btnAddGuest;
+    public ComboBox comboSelectGuest;
+
+    public void AddGuestOnAction(ActionEvent actionEvent) {
+    }
+
+    public void MAINTENANCEOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ReserveRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void manageReservationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void BackOnActio9n(ActionEvent actionEvent) {
+    }
+}
