f2c05228246e28f3a8cf9d7a458a840461daea30
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-24 22:14:29 +0530

    LogInReceptionistController Class Create

---
+++ Room_Reservation_System/src/controller/LogInReceptionistController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
+public class LogInReceptionistController {
+    public PasswordField txtReceptionUserName;
+    public Button btnCancel;
+    public Button btnLogin;
+    public PasswordField txtReceptionPassword;
+
+    public void ReceptionistLogInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelReceptionistOnAction(ActionEvent actionEvent) {
+    }
+}
