6d824035c767eb634cd97ef49313fa48f85b0829
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-26 00:15:41 +0530

    Home page Controller content added

--- Room_Reservation_System/src/controller/HomePageController.java
+++ Room_Reservation_System/src/controller/HomePageController.java
@@ -1,15 +1,26 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomePageController {
     public Button btnAdmin;
     public Button btnReceipt;
 
-    public void AdminOnActiojnh(ActionEvent actionEvent) {
+    public void AdminOnActiojnh(ActionEvent actionEvent) throws IOException {
+        Stage primaryStage=new Stage();
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LogInAdmin.fxml"))));
+        primaryStage.show();
     }
 
-    public void ReceptionistOnAction(ActionEvent actionEvent) {
+    public void ReceptionistOnAction(ActionEvent actionEvent) throws IOException {
+        Stage primaryStage=new Stage();
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LogInReceptionist.fxml"))));
+        primaryStage.show();
     }
 }
