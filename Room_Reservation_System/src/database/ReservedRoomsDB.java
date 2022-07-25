952ff063c76d418f83eac7f7770fa216547f9f72
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-26 00:05:43 +0530

    Added reservedRooms Database

---
+++ Room_Reservation_System/src/database/ReservedRoomsDB.java
@@ -0,0 +1,10 @@
+package database;
+
+import model.ReservedRooms;
+
+import java.util.ArrayList;
+
+public class ReservedRoomsDB {
+    public static ArrayList<ReservedRooms> reservedRooms=new ArrayList<>();
+
+}
